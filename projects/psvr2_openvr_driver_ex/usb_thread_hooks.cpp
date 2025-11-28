b4d8f85144545e9d08d55805d6b8c709f4e8cb49
whatdahopper <ItsKaitlyn03@protonmail.com>
2025-11-28 07:43:26 -0500

    Implement gaze user calibration commands
    Lets see how quickly I can get gaze calibration working

--- projects/psvr2_openvr_driver_ex/usb_thread_hooks.cpp
+++ projects/psvr2_openvr_driver_ex/usb_thread_hooks.cpp
@@ -6,12 +6,26 @@
 
 namespace psvr2_toolkit {
 
-  int (*CaesarUsbThread__report)(void *thisptr, bool bIsSet, uint16_t reportId, void *buffer, uint16_t length, uint16_t value, uint16_t index, uint16_t subcmd);
+#pragma pack(push, 1)
+
+  struct report_gaze_user_calibration_payload {
+    float x;
+    float y;
+    float z;
+    uint8_t value;
+  };
+
+#pragma pack(pop)
+
+  void* imuStatusUsbThread = nullptr;
+
+  int (*CaesarUsbThread__report)(void *thisptr, uint8_t reportType, uint16_t reportId, void *payload, uint16_t payloadLength, uint16_t value, uint16_t index, uint16_t command);
 
   int (*CaesarUsbThreadImuStatus__poll)(void *) = nullptr;
   int CaesarUsbThreadImuStatus__pollHook(void *thisptr) {
+    imuStatusUsbThread = thisptr;
     int result = CaesarUsbThreadImuStatus__poll(thisptr);
-    CaesarUsbThread__report(thisptr, true, 12, nullptr, 0, 0, 0, 1); // Keep gaze enabled
+    gazeStreamSetEnabled(true); // Keep gaze stream enabled
     return result;
   }
 
@@ -26,6 +40,74 @@ namespace psvr2_toolkit {
                            reinterpret_cast<void *>(CaesarUsbThreadImuStatus__pollHook),
                            reinterpret_cast<void **>(&CaesarUsbThreadImuStatus__poll));
     }
+  }
+
+  void gazeStreamGetStatus() {}
+
+  void gazeStreamSetEnabled(bool value) {
+    if (imuStatusUsbThread) {
+      CaesarUsbThread__report(imuStatusUsbThread, true, 12, nullptr, 0, 0, 0, value ? 1 : 2);
+    }
+  }
+
+  // TODO
+  void gazeUserCalibrationGetStatus() {}
+
+  void gazeUserCalibrationStart() {
+    if (imuStatusUsbThread) {
+      report_gaze_user_calibration_payload payload = {};
+      CaesarUsbThread__report(imuStatusUsbThread, true, 13, &payload, sizeof(payload), 0, 0, 1);
+    }
+  }
+
+  void gazeUserCalibrationCollect(float x, float y, float z) {
+    if (imuStatusUsbThread) {
+      report_gaze_user_calibration_payload payload = {};
+      payload.x = x;
+      payload.y = y;
+      payload.z = z;
+      CaesarUsbThread__report(imuStatusUsbThread, true, 13, &payload, sizeof(payload), 0, 0, 2);
+    }
+  }
+
+  void gazeUserCalibrationDiscard(float x, float y, float z) {
+    if (imuStatusUsbThread) {
+      report_gaze_user_calibration_payload payload = {};
+      payload.x = x;
+      payload.y = y;
+      payload.z = z;
+      CaesarUsbThread__report(imuStatusUsbThread, true, 13, &payload, sizeof(payload), 0, 0, 3);
+    }
+  }
+
+  void gazeUserCalibrationComputeAndApply() {
+    if (imuStatusUsbThread) {
+      report_gaze_user_calibration_payload payload = {};
+      CaesarUsbThread__report(imuStatusUsbThread, true, 13, &payload, sizeof(payload), 0, 0, 4);
+    }
+  }
+
+  // TODO
+  void gazeUserCalibrationGetData() {}
+
+  void gazeUserCalibrationStop() {
+    if (imuStatusUsbThread) {
+      report_gaze_user_calibration_payload payload = {};
+      CaesarUsbThread__report(imuStatusUsbThread, true, 13, &payload, sizeof(payload), 0, 0, 6);
+    }
+  }
+
+  void gazeUserCalibrationSetEnabledEye(uint8_t value) {
+    if (imuStatusUsbThread) {
+      report_gaze_user_calibration_payload payload = {};
+      payload.value = value;
+      CaesarUsbThread__report(imuStatusUsbThread, true, 13, &payload, sizeof(payload), 0, 0, 7);
+    }
+  }
+
+  void gazeUserCalibrationSetPoint(float x, float y, float z) {
+    // from reversed sceHmd2 function.
+    gazeUserCalibrationCollect(x * 1000.0f, y * 1000.0f, z * 1000.0f);
   }
 
 } // psvr2_toolkit
--- projects/psvr2_openvr_driver_ex/usb_thread_hooks.h
+++ projects/psvr2_openvr_driver_ex/usb_thread_hooks.h
@@ -7,4 +7,23 @@ namespace psvr2_toolkit {
     static void InstallHooks();
   };
 
+  // Stream
+
+  void gazeStreamGetStatus();
+  void gazeStreamSetEnabled(bool value);
+
+  // User calibration
+
+  void gazeUserCalibrationGetStatus();
+  void gazeUserCalibrationStart();
+  void gazeUserCalibrationCollect(float x, float y, float z);
+  void gazeUserCalibrationDiscard(float x, float y, float z);
+  void gazeUserCalibrationComputeAndApply();
+  void gazeUserCalibrationGetData();
+  void gazeUserCalibrationStop();
+  void gazeUserCalibrationSetEnabledEye(uint8_t value);
+
+  // Wrapper around gazeUserCalibrationCollect.
+  void gazeUserCalibrationSetPoint(float x, float y, float z);
+
 } // psvr2_toolkit
