be39a4246f728cc122c96e418224b8056ff925cf
whatdahopper <ItsKaitlyn03@protonmail.com>
2026-03-23 22:14:28 -0400

    Custom ShareManager implementation attempt

--- projects/libcustomshare/custom_share_manager.cpp
+++ projects/libcustomshare/custom_share_manager.cpp
@@ -1,5 +1,7 @@
 #include "custom_share_manager.h"
 
+#include <windows.h>
+
 CustomShareManager *CustomShareManager::m_pInstance = nullptr;
 
 void CustomShareManager::createSingleton() {
@@ -25,7 +27,29 @@ CustomShareManager *CustomShareManager::getSingleton() {
 }
 
 void CustomShareManager::initialize() {
-  const char *shareNames[2][2] = {
-    {"CUSTOM_SHARE_VRT2_WIN_GAZE_EVT", "CUSTOM_SHARE_VRT2_WIN_GAZE_MTX"}
+  // If you add more handle names, you must also increase the size of m_handles.
+  const char *handleNames[1][2] = {
+    {"CUSTOM_SHARE_VRT2_WIN_GAZE_STATUS_EVT", "CUSTOM_SHARE_VRT2_WIN_GAZE_STATUS_MTX"}
   };
+
+  for (size_t i = 0; i < sizeof(handleNames) / sizeof(handleNames[0]); i++) {
+    m_handles[i].hEvent = CreateEventA(NULL, TRUE, FALSE, handleNames[i][0]);
+    m_handles[i].hMutex = CreateMutexA(NULL, FALSE, handleNames[i][1]);
+  }
+
+  m_hFileMap = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 0x2000000, "CUSTOM_SHARE_VRT2_WIN");
+  m_pBufferData = static_cast<BufferData*>(MapViewOfFile(m_hFileMap, FILE_MAP_WRITE, 0, 0, 0x2000000));
+}
+
+void CustomShareManager::setGazeStatus(unsigned char* pGazeStatus) {
+  WaitForSingleObject(m_handles[0].hMutex, INFINITE);
+  memcpy(&m_pBufferData->gazeStatus, pGazeStatus, 0x148);
+  ReleaseMutex(m_handles[0].hMutex);
+  SetEvent(m_handles[0].hEvent);
+}
+
+void CustomShareManager::getGazeStatus(unsigned char* pGazeStatus) {
+  WaitForSingleObject(m_handles[0].hMutex, INFINITE);
+  memcpy(pGazeStatus, &m_pBufferData->gazeStatus, 0x148);
+  ReleaseMutex(m_handles[0].hMutex);
 }
--- projects/libcustomshare/custom_share_manager.h
+++ projects/libcustomshare/custom_share_manager.h
@@ -5,9 +5,24 @@ public:
   static void createSingleton();
   static CustomShareManager *getSingleton();
 
+  void setGazeStatus(unsigned char* pGazeStatus);
+  void getGazeStatus(unsigned char* pGazeStatus);
+
 private:
+  struct HandlePair {
+    void* hEvent;
+    void* hMutex;
+  };
+  struct BufferData {
+    unsigned char gazeStatus[0x148];
+  };
+
   static CustomShareManager *m_pInstance;
   static bool m_initialized;
+
+  HandlePair m_handles[1];
+  void* m_hFileMap;
+  BufferData* m_pBufferData;
 
   void initialize();
 };
