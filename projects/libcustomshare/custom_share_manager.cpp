fc50b638422cfcfb38ef2963a3611342394cf4c5
whatdahopper <ItsKaitlyn03@protonmail.com>
2026-03-25 00:15:43 -0400

    Add temporary gaze image share code
    TODO: Circular buffer

--- projects/libcustomshare/custom_share_manager.cpp
+++ projects/libcustomshare/custom_share_manager.cpp
@@ -29,8 +29,9 @@ CustomShareManager *CustomShareManager::getSingleton() {
 
 void CustomShareManager::initialize() {
   // If you add more handle names, you must also increase the size of m_handles.
-  const char *handleNames[1][2] = {
-    {"CUSTOM_SHARE_VRT2_WIN_GAZE_STATUS_EVT", "CUSTOM_SHARE_VRT2_WIN_GAZE_STATUS_MTX"}
+  const char *handleNames[2][2] = {
+    {"CUSTOM_SHARE_VRT2_WIN_GAZE_STATUS_EVT", "CUSTOM_SHARE_VRT2_WIN_GAZE_STATUS_MTX"},
+    {"CUSTOM_SHARE_VRT2_WIN_GAZE_IMAGE_EVT", "CUSTOM_SHARE_VRT2_WIN_GAZE_IMAGE_MTX"}
   };
 
   for (size_t i = 0; i < sizeof(handleNames) / sizeof(handleNames[0]); i++) {
@@ -54,3 +55,45 @@ void CustomShareManager::getGazeStatus(unsigned char* pGazeStatus) {
   memcpy(pGazeStatus, &m_pBufferData->gazeStatus, 0x148);
   ReleaseMutex(m_handles[0].hMutex);
 }
+
+void CustomShareManager::setGazeImage(unsigned char* pGazeImage) {
+  WaitForSingleObject(m_handles[1].hMutex, INFINITE);
+  memcpy(&m_pBufferData->gazeImage, pGazeImage, 0x200100);
+  ReleaseMutex(m_handles[1].hMutex);
+  SetEvent(m_handles[1].hEvent);
+}
+
+void CustomShareManager::getGazeImage(unsigned char* pGazeImage) {
+  WaitForSingleObject(m_handles[1].hMutex, INFINITE);
+  memcpy(pGazeImage, &m_pBufferData->gazeImage, 0x200100);
+  ReleaseMutex(m_handles[1].hMutex);
+}
+
+/*int CustomShareManager::getGazeImageBuffer(unsigned char** gazeImageBuffer, unsigned char** a2) {
+  WaitForSingleObject(m_handles[1].hMutex, INFINITE);
+  int result = -1;
+  int i = 0;
+  int gazeImageIndex = 0;
+  int gazeImageCount = gazeImageCounter + 1;
+  while (1) {
+    gazeImageIndex = gazeImageCount % 8;
+    if (*(int *)&m_pBufferData->bufferInfo[0x878 * (gazeImageCount % 8)] <= 1) {
+      break;
+    }
+    ++i;
+    ++gazeImageCount;
+    if (i >= 8) {
+      ReleaseMutex(m_handles[1].hMutex);
+      return result;
+    }
+  }
+  result = gazeImageCount % 8;
+  if (gazeImageIndex >= 0) {
+    int bufferInfoOffset = 0x878 * gazeImageIndex;
+    *gazeImageBuffer = &m_pBufferData->gazeImage[0x200100 * gazeImageIndex];
+    *(int *)&m_pBufferData->bufferInfo[bufferInfoOffset] = 3;
+    *a2 = &m_pBufferData->bufferInfo[bufferInfoOffset + 12];
+  }
+  ReleaseMutex(m_handles[1].hMutex);
+  return result;
+}*/
--- projects/libcustomshare/custom_share_manager.h
+++ projects/libcustomshare/custom_share_manager.h
@@ -8,6 +8,11 @@ public:
   void setGazeStatus(unsigned char* pGazeStatus);
   void getGazeStatus(unsigned char* pGazeStatus);
 
+  void setGazeImage(unsigned char* pGazeImage);
+  void getGazeImage(unsigned char* pGazeImage);
+
+  //int getGazeImageBuffer(unsigned char** gazeImageBuffer, unsigned char** a2);
+
 private:
   struct HandlePair {
     void* hEvent;
@@ -15,14 +20,17 @@ private:
   };
   struct BufferData {
     unsigned char gazeStatus[0x148];
+    //unsigned char bufferInfo[0x878 * 8]; // TODO: Circular buffer
+    unsigned char gazeImage[0x200100/* * 8*/];
   };
 
   static CustomShareManager *m_pInstance;
   static bool m_initialized;
 
-  HandlePair m_handles[1];
+  HandlePair m_handles[2];
   void* m_hFileMap;
   BufferData* m_pBufferData;
+  //int gazeImageCounter;
 
   void initialize();
 };
