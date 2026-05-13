32864403b7e99c4b4e8ed1ec589ebd96aa373edd
whatdahopper <ItsKaitlyn03@protonmail.com>
2026-05-13 00:06:00 -0400

    A bunch of ShareManager stubs

--- projects/psvr2_openvr_driver_ex/driver_hooks/share_manager_hooks.cpp
+++ projects/psvr2_openvr_driver_ex/driver_hooks/share_manager_hooks.cpp
@@ -7,8 +7,34 @@ namespace psvr2_toolkit {
   void ShareManager__createSingletonHook(int a1) {
     static HmdDriverLoader *pHmdDriverLoader = HmdDriverLoader::Instance();
 
-    *reinterpret_cast<bool*>(pHmdDriverLoader->GetBaseAddress() + 0x354F80) = true; // ShareManager::m_initialized
-    *reinterpret_cast<uintptr_t*>(pHmdDriverLoader->GetBaseAddress() + 0x354F78) = 0xCAFEBABE; // ShareManager::m_pInstance
+    *reinterpret_cast<bool *>(pHmdDriverLoader->GetBaseAddress() + 0x354F80) = true; // ShareManager::m_initialized
+    *reinterpret_cast<uintptr_t *>(pHmdDriverLoader->GetBaseAddress() + 0x354F78) = 0xCAFEBABE; // ShareManager::m_pInstance
+  }
+
+  uintptr_t ShareManager__getSingletonHook() {
+    return 0xCAFEBABE;
+  }
+
+  void ShareManager__sub_18015E2A0Hook(void *thisptr) {
+    // This function is run on startup, it sets a DWORD to 0.
+    // TODO: Reverse this.
+  }
+
+  // Hopefully no register stomping...
+  void ShareManager__sub_18015E990Hook(void *thisptr, void* a2) {
+    // This function is run inside CaesarUsbThreadImuStatus::initialize, it appears to copy a buffer.
+    // TODO: Reverse this.
+  }
+
+  void ShareManager__sub_18015F330Hook(void *thisptr, void* a2) {
+    // This function is run inside CaesarUsbThreadImuStatus::initialize, it sets a few variables.
+    // TODO: Reverse this.
+  }
+
+  // Some config thing.
+  void ShareManager__sub_18015B950Hook(void *thisptr, int configId, int *outval) {
+    // This function is run inside CaesarUsbThreadImuStatus::initialize, it gets a config variable.
+    // TODO: Reverse this.
   }
 
   void ShareManagerHooks::InstallHooks() {
@@ -17,6 +43,26 @@ namespace psvr2_toolkit {
     // ShareManager::createSingleton
     HookLib::InstallHook(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x15BCF0),
                          reinterpret_cast<void *>(ShareManager__createSingletonHook));
+
+    // ShareManager::getSingleton
+    HookLib::InstallHook(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x15BBD0),
+                         reinterpret_cast<void *>(ShareManager__getSingletonHook));
+
+    // ShareManager::sub_18015E2A0
+    HookLib::InstallHook(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x15E2A0),
+                         reinterpret_cast<void *>(ShareManager__sub_18015E2A0Hook));
+
+    // ShareManager::sub_18015E990
+    HookLib::InstallHook(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x15E990),
+                         reinterpret_cast<void *>(ShareManager__sub_18015E990Hook));
+
+    // ShareManager::sub_18015F330
+    HookLib::InstallHook(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x15F330),
+                         reinterpret_cast<void *>(ShareManager__sub_18015F330Hook));
+
+    // ShareManager::sub_18015B950
+    HookLib::InstallHook(reinterpret_cast<void *>(pHmdDriverLoader->GetBaseAddress() + 0x15B950),
+                         reinterpret_cast<void *>(ShareManager__sub_18015B950Hook));
   }
 
 }
